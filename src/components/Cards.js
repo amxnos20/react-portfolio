aad1eb335bfdaeebf7f6c0fe0fc620248d3d495e
Aman <amxnos@gmail.com>
2021-08-06 09:05:14 +0100

    changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,7 @@ import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
 import Title from './Title'
-import netflixClone from '../images/netflix-clone.PNG'
+import uberClone from '../images/uber-clone.jpg'
 import amazonClone from '../images/amazon-next.PNG'
 import covidTracker from '../images/covid-tracker.PNG'
 
@@ -23,25 +23,25 @@ const Cards = () => {
       <div className='row'>
         <CardItem
           src={covidTracker}
-          title='Covid Tracker'
-          description='Covid App built with real data, map, chart, table... functionality. Tech: React,Custom API,Material-UI,Leaflet'
+          title='Covid Tracker Web App'
+          description='👉 Covid App built with real data, map, chart, table... functionality. Tech: React, Custom API, Material-UI, Map Leaflet'
           onClick={() =>
             openUrlInNewTab('https://covid-tracker-5d6f2.web.app/')
           }
         />
         <CardItem
           src={amazonClone}
-          title='Amazon Clone Demo App'
-          description='Amazon App with full user authentification, basket functionality, database and stripe payment system. Tech: React,NextJS,Redux,Tailwind CSS,Custom API,Webhook,Stripe,Firebase'
-          onClick={() => openUrlInNewTab('https://amazon-2-0-delta.vercel.app/')}
+          title='Amazon Clone Web App'
+          description='👉 Amazon App with full user authentification, basket functionality, database and stripe payment system. Tech: React, NextJS, Redux, Tailwind CSS, Custom API, Webhook, Stripe, Firebase'
+          onClick={() =>
+            openUrlInNewTab('https://amazon-2-0-delta.vercel.app/')
+          }
         />
         <CardItem
-          src={netflixClone}
-          title='Netflix Clone Demo App'
-          description='Netflix App with full user authentification, database and stripe payment system.'
-          onClick={() =>
-            openUrlInNewTab('https://netflix-clone-7491d.web.app/')
-          }
+          src={uberClone}
+          title='Uber Clone Mobile App (not published but working locally)'
+          description='
+          👉 Tech: React Native, Tailwind CSS with RN, Google Distance Matrix API to calculate Travel time and Distance (+ Cost), Directions Google API for real navigation, Google places API for real navigations, Apple & google maps for iOS & Android, React Native Navigation'
         />
       </div>
     </div>
